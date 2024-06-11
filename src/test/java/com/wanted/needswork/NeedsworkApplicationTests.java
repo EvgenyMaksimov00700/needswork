4f8b04681878965e5713c353aab767aa3793d4f4
EvgenyMaksimov00700 <maksiel1983@icloud.com>
2024-06-11 15:42:42 +0300

    Initial Commit

---
+++ src/test/java/com/wanted/needswork/NeedsworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanted.needswork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeedsworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
