14c7fb1703fa628c14a69aea9eb47c16a004bb8e
EvgenyMaksimov00700 <maksiel1983@icloud.com>
2024-06-14 13:26:18 +0300

    createProjectStructure

--- src/main/java/com/wanted/needswork/models/Employer.java
+++ src/main/java/com/wanted/needswork/models/Employer.java
@@ -2,9 +2,11 @@ package com.wanted.needswork.models;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@NoArgsConstructor
 public class Employer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,5 +16,36 @@ public class Employer {
 
     @Getter
     @Setter
-    private String username;
+    private Integer user_id;
+
+    @Getter
+    @Setter
+    private Integer inn;
+
+    @Getter
+    @Setter
+    private Integer ogrn;
+
+    @Getter
+    @Setter
+    private String name;
+
+    @Getter
+    @Setter
+    private String logo;
+
+    @Getter
+    @Setter
+    private String description;
+
+    public Employer (Integer id, Integer user_id, Integer inn, Integer ogrn, String name, String logo, String description) {
+        this.id = id;
+        this.user_id = user_id;
+        this.inn = inn;
+        this.ogrn = ogrn;
+        this.logo = logo;
+        this.description = description;
+
+
+    }
 }
