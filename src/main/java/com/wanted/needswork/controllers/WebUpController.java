35764e8521c5f2d677fcc90d368db480dfeca750
EvgenyMaksimov00700 <maksiel1983@icloud.com>
2024-09-05 15:37:40 +0300

    add lk_employer.js

--- src/main/java/com/wanted/needswork/controllers/WebUpController.java
+++ src/main/java/com/wanted/needswork/controllers/WebUpController.java
@@ -9,24 +9,34 @@ public class WebUpController {
         return "reg_employer";
 
     }
+
     @GetMapping("/employer/lk/")
     public String getEmployerLk() {
         return "lk_employer";
     }
+
     @GetMapping("/employer/vacancy/create")
     public String getCreateVacancyForm() {
         return "create_vacancy_form";
     }
+
     @GetMapping("/employer/my_vacancy7/show")
     public String getMyVacancy7() {
         return "my_vacancy7";
     }
+
     @GetMapping("/employer/reg_employer1/reg")
     public String getRegEmployer1() {
         return "reg_employer1";
     }
+
     @GetMapping("/employer/responses7/show")
     public String getResponses7() {
         return "responses7";
+    }
+
+    @GetMapping("/employer/vacancy/description")
+    public String getDescription() {
+        return "description_vacancy";
     }
 }
---
+++ src/main/resources/static/js/my_vacancy7.js
@@ -0,0 +1,33 @@
+//let clientID = window.Telegram.WebApp.initDataUnsafe.user.id;
+let clientID = 159619887
+console.log(clientID)
+document.addEventListener('DOMContentLoaded', function(){
+     fetch( `/vacancy/user/${clientID}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(response => {
+                if (!response.ok) {
+
+                    throw new Error(`Ошибка HTTP: ${response.status}`); // Бросаем ошибку, если ответ не в порядке
+                }
+                return response.json();
+
+            }).then(data => {
+            const vacancies=document.getElementById("vacancies")
+            data.forEach (vacancy => {
+            let salary="";
+            if (vacancy.fromSalary!=null) {
+            salary+=`от ${vacancy.fromSalary}`;}
+            if (vacancy.toSalary!=null) {
+            salary+=` до ${vacancy.toSalary}`;
+            }
+            if (salary=="") {
+            salary="Не указано";}
+            const element= `<button class="vacancy" onclick="window.location.href='/employer/vacancy/description'">
+                                    ${vacancy.position}<br>${salary}<br>${vacancy.city}<br>${vacancy.employer.name}
+                                </button>`
+                                 vacancies.innerHTML+=element})
+            })
+});
