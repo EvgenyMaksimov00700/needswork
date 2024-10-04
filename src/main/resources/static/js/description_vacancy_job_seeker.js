338f0d15d05855c712cfb46ed8078619ef0220b4
EvgenyMaksimov00700 <maksiel1983@icloud.com>
2024-10-04 17:43:58 +0300

    add description_vacancy.js

---
+++ src/main/resources/static/js/description_vacancy_job_seeker.js
@@ -0,0 +1,68 @@
+const currentUrl = window.location.href;
+const url = new URL(currentUrl);
+const params = new URLSearchParams(url.search);
+const vacancyId = params.get('id');
+function edit_vacancy () {
+window.location.href=`/employer/vacancy/edit?id=${vacancyId}`}
+function vacancy_responses() {
+window.location.href=`/employer/responses7/show?id=${vacancyId}`}
+
+function delete_vacancy() {
+if (confirm("Вы действительно хотите удалить вакансию?")){
+fetch( `/vacancy/${vacancyId}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(response => {
+                if (!response.ok) {
+
+                    throw new Error(`Ошибка HTTP: ${response.status}`);
+                }
+                return response.json();
+
+            }).then(data => {window.location.href="/employer/my_vacancy7/show"})
+
+}
+}
+document.addEventListener('DOMContentLoaded', function(){
+     fetch( `/vacancy/${vacancyId}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(response => {
+                if (!response.ok) {
+
+                    throw new Error(`Ошибка HTTP: ${response.status}`);
+                }
+                return response.json();
+
+            }).then(data => {
+                console.log (data);
+                document.getElementById("position").innerHTML = data.position;
+                document.getElementById("employer_name").innerHTML = "<b>Компания: </b>" + data.employer.name;
+                document.getElementById("city").innerHTML = "<b>Город: </b>" + data.city;
+                document.getElementById("address").innerHTML = "<b>Адрес: </b>" + data.address;
+                document.getElementById("salary").innerHTML += "<b>Зарплата: </b>";
+                if (data.fromSalary!=null) {
+                document.getElementById("salary").innerHTML += "от " + data.fromSalary + " ";
+                }
+                if (data.toSalary!=null) {
+                document.getElementById("salary").innerHTML += "до " + data.toSalary + " ";
+                }
+                if (data.fromSalary==null && data.toSalary==null) {
+                document.getElementById("salary").innerHTML += "не указана";
+                }
+                else {
+                document.getElementById("salary").innerHTML += "руб";}
+                document.getElementById("exp").innerHTML = "<b>Требуемый опыт работы: </b>" + data.exp;
+                document.getElementById("workSchedule").innerHTML = "<b>График работы: </b>" + data.workSchedule;
+                if (data.distantWork==true) {
+                document.getElementById("workSchedule").innerHTML += ", возможно удаленно";}
+                document.getElementById("responsibility").innerHTML = data.responsibility;
+                document.getElementById("create_date").innerHTML = "<b>Дата публикации: </b>" + data.createdDateTime;
+            });
+
+});
+
