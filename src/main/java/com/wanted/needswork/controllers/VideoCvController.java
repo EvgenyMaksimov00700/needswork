21d3c213bb00d7ec26a55deeecd3cd22209adac3
EvgenyMaksimov00700 <maksiel1983@icloud.com>
2024-10-22 17:44:13 +0300

    add description_vacancy.js

--- src/main/java/com/wanted/needswork/controllers/VideoCvController.java
+++ src/main/java/com/wanted/needswork/controllers/VideoCvController.java
@@ -13,10 +13,11 @@ import com.wanted.needswork.services.VideoCvService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@Controller()
 public class VideoCvController {
     @Autowired
     JobSeekerService jobSeekerService;
