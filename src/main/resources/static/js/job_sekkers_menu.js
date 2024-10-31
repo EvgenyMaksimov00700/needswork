7f92529b7128a3ce4426f32c06d28438686e60d5
EvgenyMaksimov00700 <maksiel1983@icloud.com>
2024-10-31 17:29:21 +0300

    add description_vacancy.js

--- src/main/resources/static/js/job_sekkers_menu.js
+++ src/main/resources/static/js/job_sekkers_menu.js
@@ -12,3 +12,47 @@ window.Telegram.WebApp.openTelegramLink(`https://t.me/Cherchent`); // URL с ID 
                 window.Telegram.WebApp.close();
                 }
 }
+
+document.addEventListener('DOMContentLoaded', function(){
+     fetch( `/videoCv/user/${clientID}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(response => {
+                if (!response.ok) {
+
+                    throw new Error(`Ошибка HTTP: ${response.status}`); // Бросаем ошибку, если ответ не в порядке
+                }
+                return response.json();
+
+            }).then(data => {
+            const vacancies=document.getElementById("resume-buttons")
+            data.forEach (videoCv => {
+
+            const vacancy_url = "window.location.href='/employer/vacancy/description?id=" + vacancy.id + "'";
+            const element= `<div> class="resume-button" onclick=${vacancy_url}>
+                                    ${videoCv.name}
+                                </div>`
+                                 vacancies.innerHTML+=element})
+            })
+});
+function delete_employer() {
+if (confirm("Вы действительно хотите удалить Ваш аккаунт?")){
+fetch( `/employer/${employerId}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(response => {
+                if (!response.ok) {
+
+                    throw new Error(`Ошибка HTTP: ${response.status}`);
+                }
+                return response.json();
+
+            }).then(data => {window.Telegram.WebApp.openTelegramLink(`https://t.me/tworker_ru_bot?start=start`);
+                             window.Telegram.WebApp.close();})
+
+}
+}
